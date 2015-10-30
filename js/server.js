ad0ab5864bbf27f83ca08b2c6817c3bd2c5004b2
root <root@homebox.(none)>
2015-10-29 23:38:14 -0400

    server.js changes

--- js/server.js
+++ js/server.js
@@ -5,16 +5,21 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // MongoDB
-mongoose.connect('mongodb://localhost/rest_test')
+mongoose.connect('mongodb://localhost:27999/rest_test')
 
 // Express
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 // Routes
 app.use('/api', require('./routes/api'));
 
 // Start server
-app.listen(3000);
-console.log('API is running on port 3000');
+app.listen(8999);
+console.log('API is running on port 8999');
